7d378f308d7b0c099b42d41d8e7b521893f0dfce
Hasan Küçükselek <haskselek@hotmail.com>
2024-04-29 16:14:58 +0300

    second commit

---
+++ src/P02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adinizi giriniz : ");
+        String isim = sc.nextLine();
+    }
+}
