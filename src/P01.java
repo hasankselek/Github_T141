f9f2fd84e279982082e19e9608d5ed7ff6bdc74c
Hasan Küçükselek <haskselek@hotmail.com>
2024-04-29 16:03:01 +0300

    first commit

---
+++ src/P01.java
@@ -0,0 +1,9 @@
+public class P01 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Github");
+        int a = 120;
+        int b = 180;
+        int c = a + b;
+        System.out.println("c = " + c);
+    }
+}
